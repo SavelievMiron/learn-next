34d0c42d81c9955678572434b8111d14d0295c36
SavelievMiron <savelevmiron@gmail.com>
2021-12-07 13:04:10 +0200

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
 import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
 import Image from 'next/image'
 import styles from '../styles/Layout.module.css'
 
-export default function Home() {
+export default function Home({articles}) {
   return (
     <div>
-      <h1>Welcome to Next.js</h1>
+      <Head>
+        <title>WebDev Newz</title>
+        <meta name='keywords' content='web development, programming' />
+      </Head>
+
+      <ArticleList articles={articles} />
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
